e0b6993d9f6f24d363d07b87ad4a1597181b2798
Vger <vcdelariva@gmail.com>
2018-08-03 17:16:22 -0500

    Read form

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,7 +6,7 @@ var friendArray = [
     {
       friendName: "Napoleon Dynamite",
       photo: "https://pbs.twimg.com/profile_images/976941155370795014/ZzdOhH9C_400x400.jpg",
-      scores: []
+      scores: [5,5,5,5,5,5,5,5,5,5]
     }
   ];
   
